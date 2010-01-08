f59283ef52a86bc48966a247704a3ef717342aca
alex.antone <alex.antone@3565feb8-b4c8-11de-b19d-3371f2377dbd>
2010-01-08 05:34:42 +0000

    Author: alex.antone Description: Improved start.sh (include usage message)
    Test:
            ./start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 NPROC=`head -n1 dme.conf | cut -d ' ' -f 1`
+
 ALGORITHM=lamport
-[ -n "$1" ] && ALGORITHM="$1"
+SUPERVISOR_ARGS="-f dme.conf -t 30 -r 70 -o supervisor.log"
+
+USAGEMSG="Usage:\n"\
+"       start.sh   [algorithm]\n"\
+"                  [-f <config-file>] [-r <concurency ratio>] [-t <sec interval>]\n"\
+"                  [-o <out-logfile>]\n\n"\
+"By default the algorithm is lamport. See build/ for other algorithms.\n"\
+"Default parameter for supervisor are:\n\t'$SUPERVISOR_ARGS'\n"
+
+
+[ "$1" == "-h" ] && {
+    echo -e "$USAGEMSG"
+    exit
+}
+
+[ -n "$1" ] &&  {
+    ALGORITHM="$1"
+    shift 1
+}
+
+[[ "$#" -gt 0 ]] && SUPERVISOR_ARGS="$@"
 
 for (( ix = 1; ix <= NPROC; ix++ )) ; do
         echo "Starting process $ix ..."
@@ -12,4 +33,4 @@ for (( ix = 1; ix <= NPROC; ix++ )) ; do
 done
 
 echo "Starting supervisor ..."
-xterm -geometry 140x20 -T "Supervisor" -e "./build/supervisor -f dme.conf -t 30 -r 70; read -p'--exited--'" &
+xterm -geometry 140x20 -T "Supervisor" -e "./build/supervisor $SUPERVISOR_ARGS ; read -p'--exited--'" &
